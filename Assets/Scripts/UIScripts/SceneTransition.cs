022e81d82ba0007f83c765e7d19199ac2504d829
Emma Schmitz <schmitz.emma@gmx.de>
2022-05-26 18:41:07 +0200

    no message

--- Assets/Scripts/UIScripts/SceneTransition.cs
+++ Assets/Scripts/UIScripts/SceneTransition.cs
@@ -87,6 +87,10 @@ public class SceneTransition : MonoBehaviour
             PlayerController.Instance.IsInCombat = false;
             playerSword.enabled = false;
         }
+        if(_newScene == SceneManager.GetSceneByName("MainRoom"))
+        {
+            PlayerController.Instance.Revive();
+        }
         StartCoroutine(FinishTransition());
     }
 
